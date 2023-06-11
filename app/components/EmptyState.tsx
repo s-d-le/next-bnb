91c343bff97b0712ffcec4b646f6405f65ded21f
Son Le <ayzkeys@gmail.com>
2023-06-11 23:34:09 +0200

    empty state page

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import Heading from "./Heading";
+import Button from "./Button";
+
+interface EmptyStateProps {
+  title?: string;
+  subTitle?: string;
+  showReset?: boolean;
+}
+
+const EmptyState: React.FC<EmptyStateProps> = ({
+  title = "No results found",
+  subTitle = "Try adjusting your search or filter to find what you're looking for.",
+  showReset = false,
+}) => {
+  const router = useRouter();
+  return (
+    <div className="flex h-[60vh] flex-col items-center justify-center gap-2">
+      <Heading center title={title} subtitle={subTitle} />
+      <div className="mt-4 w-48">
+        {showReset && (
+          <Button
+            outline
+            label="Remove all filters"
+            onClick={() => router.push("/")}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default EmptyState;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import Container from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
-  return <div className="text-2xl text-rose-500">Hello Airbnb</div>;
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>My listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
