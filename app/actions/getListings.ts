de098f5d18bf64a725cb0d940a08bb05ec0838f0
Son Le <ayzkeys@gmail.com>
2023-07-01 23:31:13 +0200

    filters action

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,21 +1,85 @@
 /**
- * Get all listings for landing page and current users properties
+ * Get all listings for landing page, search results and current users properties
  */
 import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const {userId} = params;
+    const {
+      userId,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
     let query: any = {};
 
     //If userId is provided, then filter by userId
-    if(userId) {
+    if (userId) {
       query.userId = userId;
+    }
+
+    //The rest is for search
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount, //gte = greater than or equal to
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    //If startDate and endDate is provided, then filter by date
+    if (startDate && endDate) {
+      query.NOT = {
+        //Dont include the listing if the dates overlap with the reservation
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import qs from "query-string";
+import { useState, useMemo, useCallback } from "react";
 import { Range } from "react-date-range";
+import dynamic from "next/dynamic";
 
 import { useRouter, useSearchParams } from "next/navigation";
 import Modal from "./Modal";
 
 import useSearchModal from "@/app/hooks/useSearchModal";
+import CountrySelect, { CountrySelectValue } from "../inputs/CountrySelect";
+import { formatISO } from "date-fns";
+import Heading from "../Heading";
+import Calendar from "../inputs/Calendar";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   LOCATION = 0,
@@ -19,6 +26,7 @@ const SearchModal = () => {
   const params = useSearchParams();
   const searchModal = useSearchModal();
 
+  const [location, setLocation] = useState<CountrySelectValue>();
   const [step, setStep] = useState(STEPS.LOCATION);
   const [guestCount, setGuestCount] = useState(1);
   const [roomCount, setRoomCount] = useState(1);
@@ -29,12 +37,165 @@ const SearchModal = () => {
     key: "selection",
   });
 
+  //Import map with useMemo to avoid re-rendering. Turn off ssr. Rerender only when location changes
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
+
+  const onBack = useCallback(() => {
+    setStep((value) => value - 1);
+  }, []);
+
+  const onNext = useCallback(() => {
+    setStep((value) => value + 1);
+  }, []);
+
+  const onSubmit = useCallback(async () => {
+    if (step !== STEPS.INFO) {
+      return onNext();
+    }
+
+    let currentQuery = {};
+
+    //If there params, parse them
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery, //this includes the current listing category user is in
+      locationValue: location?.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+    };
+
+    //Format date to string so they can go into the URL
+    if (dateRange.startDate) {
+      updatedQuery.startDate = formatISO(dateRange.startDate);
+    }
+
+    if (dateRange.endDate) {
+      updatedQuery.endDate = formatISO(dateRange.endDate);
+    }
+
+    //Create the final URL
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    setStep(STEPS.LOCATION);
+    searchModal.onClose();
+
+    router.push(url); //apply filters
+  }, [
+    step,
+    searchModal,
+    location,
+    router,
+    guestCount,
+    roomCount,
+    dateRange,
+    onNext,
+    bathroomCount,
+    params,
+  ]);
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.INFO) {
+      return "Search";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.LOCATION) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  //Content and values of the modal
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Where do you wanna go?"
+        subtitle="Find the perfect location!"
+      />
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <hr />
+      <Map center={location?.latlng} />
+    </div>
+  );
+
+  if (step === STEPS.DATE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="When do you plan to go?"
+          subtitle="Make sure everyone is free!"
+        />
+        <Calendar
+          onChange={(value) => setDateRange(value.selection)}
+          value={dateRange}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="More information" subtitle="Find your perfect place!" />
+        <Counter
+          onChange={(value) => setGuestCount(value)}
+          value={guestCount}
+          title="Guests"
+          subTitle="How many guests are coming?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setRoomCount(value)}
+          value={roomCount}
+          title="Rooms"
+          subTitle="How many rooms do you need?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => {
+            setBathroomCount(value);
+          }}
+          value={bathroomCount}
+          title="Bathrooms"
+          subTitle="How many bahtrooms do you need?"
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={searchModal.isOpen}
-      title="Filters"
-      actionLabel="Search"
+      title="Location Filters"
+      actionLabel={actionLabel}
+      onSubmit={onSubmit}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
       onClose={searchModal.onClose}
+      body={bodyContent}
     />
   );
 };
